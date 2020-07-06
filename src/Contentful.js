8917b1f6ad61234382522a7a17608ec82dceb1ef
ariful4966 <ariful4082@gmail.com>
2020-07-06 23:47:43 +0600

    add dotenv

--- src/Contentful.js
+++ src/Contentful.js
@@ -3,6 +3,6 @@ import { createClient } from "contentful";
 
 
 export default createClient({
- accessToken: 'HaA1qR8YLofiw43OtiHfITsrgIwdF4YBomR9kuoEwmU',
- space: 'eqhmm0gnmnaj'
+ accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+ space: process.env.REACT_APP_API_SPACE
 })
